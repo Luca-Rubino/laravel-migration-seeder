801306019da7da8900c8bd66167e27dd1f04c76f
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-24 18:28:49 +0200

    Creare rotta per trainlist

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/train_list', function () {
+    return view('component.trainList');
+})->name('trainList');
